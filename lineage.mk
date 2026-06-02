faa5ae96a2e9546de0629cd8469b03c0da8a6aea
palmbeach05 <5945226+palmbeach05@users.noreply.github.com>
2026-06-02 16:40:34 -0400

    d2att-unified: Consolidate mk files

--- lineage.mk
+++ lineage.mk
@@ -1,13 +1,31 @@
-$(call inherit-product, device/samsung/d2-unified/full_d2att.mk)
-
+# Copyright (C) 2011 The Android Open Source Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
 # Inherit some common Lineage stuff.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, device/samsung/d2-unified/device.mk)
+
+# Product Identity
+PRODUCT_NAME := lineage_d2att
+PRODUCT_DEVICE := d2att
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := SAMSUNG-SGH-I747
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2uc \
     TARGET_DEVICE=d2att \
     BUILD_FINGERPRINT="samsung/d2uc/d2att:4.4.2/KOT49H/I747UCUFNJ2:user/release-keys" \
     PRIVATE_BUILD_DESC="d2uc-user 4.4.2 KOT49H I747UCUFNJ2 release-keys"
-
-PRODUCT_NAME := lineage_d2att
-PRODUCT_DEVICE := d2att
